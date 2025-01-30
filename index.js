9d8c37df74a630ee7376aaf5484b50a68cf297f2
Michael <75168420+Boshommi@users.noreply.github.com>
2025-01-30 17:44:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -197,8 +197,8 @@ if(process.env.MTLS_ENABLE){
     }
 }
 
-var httpServer = http.createServer(app).listen(process.env.HTTP_PORT || 8080);
-var httpsServer = https.createServer(sslOpts,app).listen(process.env.HTTPS_PORT || 8443);
+var httpServer = http.createServer(app).listen(process.env.HTTP_PORT || 8080, '0.0.0.0');
+var httpsServer = https.createServer(sslOpts,app).listen(process.env.HTTPS_PORT || 8443, '0.0.0.0');
 console.log(`Listening on ports ${process.env.HTTP_PORT || 8080} for http, and ${process.env.HTTPS_PORT || 8443} for https.`);
 
 let calledClose = false;
